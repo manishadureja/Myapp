c93f58b5426e44495491b89fe299aa395a07017f
Manisha <51935220+manishadureja@users.noreply.github.com>
2021-06-29 22:25:28 +1000

    Update App.js
    commit

--- App.js
+++ App.js
@@ -1,76 +1,25 @@
 //Imports
 import React, { useState } from "react";
 import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import ToDoList from "./ToDoList";
+import Counter from "./counter";
+import ColorFlipper from "./ColorFlipper";
+import Accordion from "./Accordion";
+import OpenModal from "./OpenModal";
 
-//This component will change the background color(any random color) on the click of a button.
-const ToDoList = () => {
-  const [itemName, setItemName] = useState('');
-  const [itemList, setItemList] = useState([]);
-  const [editing, setEditing] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(-1);
 
-  //add an item to the list or edit the current item.
-  const addItem = () => {
-    if (itemName === '') {
-      alert('Item name can not be empty.');
-      return;
-    }
-    if (editing) {
-      itemList.splice(currentIndex, 1, itemName);
-      setItemList([...itemList]);
-    } else {
-      setItemList([...itemList, itemName]);
-    }
-    setItemName('');
-    setEditing(false);
-  }
-  const editItem = (index, item) => {
-    setEditing(true);
-    setCurrentIndex(index);
-    setItemName(item);
 
-  }
-
-  const deleteItem = (index) => {
-      itemList.splice(index, 1);
-      alert(itemList);
-      setItemList([...itemList]);
-  }
-  //return the view.
+//This component will invoke other components.
+const App = () => {
   return (
-    <View style={styles.container} >
-      <h1>Item List</h1>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter Item" onChangeText={text => setItemName(text)} value={itemName} />
-        &nbsp;<Button onPress={addItem} title={editing ? "Edit" : "Add"} />
-      </View>
+  <ToDoList/>
+  //<Counter/>,
+  //<ColorFlipper/>,
+  //<Accordion/>,
+  //<OpenModal/>
+  
 
-
-      <View >
-        {itemList.map((item, index) => {
-          return <View style={styles.inputContainer}><Text key={index} Text={item}>{item}</Text>&nbsp;<Button title='Edit' onPress={() => { editItem(index, item); }} />&nbsp;<Button title='Delete' onPress={() => { deleteItem(index); }} /></View>
-        })}
-      </View>
-    </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    width: '90%', height: '90%', alignItems: 'center'
-  },
-  inputContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  input: {
-    width: '90%'
-  },
-  button: {
-    width: '30%',
-    alignItems: 'center'
-  },
 
-});
-
-export default ToDoList;
+export default App; 
